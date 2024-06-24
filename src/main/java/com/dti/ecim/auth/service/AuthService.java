bbd972bd56f9386a4f8608410d23511fc93171ec
Gerry Sheva <gerry.sheva@proton.me>
2024-06-24 14:29:29 +0700

    config(cache): configured connection to redis

--- src/main/java/com/dti/ecim/auth/service/AuthService.java
+++ src/main/java/com/dti/ecim/auth/service/AuthService.java
@@ -5,6 +5,7 @@ import com.dti.ecim.auth.dto.LoginRequestDto;
 import com.dti.ecim.auth.dto.RegisterRequestDto;
 import org.apache.coyote.BadRequestException;
 import org.springframework.http.HttpHeaders;
+import org.springframework.security.core.GrantedAuthority;
 
 public interface AuthService {
     AuthResponseDto registerUser(RegisterRequestDto requestDto) throws BadRequestException;
