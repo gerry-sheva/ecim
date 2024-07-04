ffe36905486791568aaeff4e90d68fcb952dc5ad
Gerry Sheva <gerry.sheva@proton.me>
2024-07-05 03:19:44 +0700

    feat(organizer): added find by email for organizer

---
+++ src/main/java/com/dti/ecim/user/dto/OrganizerResponseDto.java
@@ -0,0 +1,12 @@
+package com.dti.ecim.user.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@Data
+@NoArgsConstructor
+public class OrganizerResponseDto {
+    private Long userId;
+}
