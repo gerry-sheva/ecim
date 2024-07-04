9e0bfd51905a5e04ce6cea908a098d67a621de7a
Gerry Sheva <gerry.sheva@proton.me>
2024-07-04 12:45:03 +0700

    refactor(trx): removed tix service and repo

--- src/main/java/com/dti/ecim/trx/repository/TixRepository.java
+++
@@ -1,7 +0,0 @@
-package com.dti.ecim.trx.repository;
-
-import com.dti.ecim.trx.entity.Tix;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TixRepository extends JpaRepository<Tix, Long> {
-}
--- src/main/java/com/dti/ecim/trx/service/TixService.java
+++
@@ -1,7 +0,0 @@
-package com.dti.ecim.trx.service;
-
-import com.dti.ecim.trx.entity.Tix;
-
-public interface TixService {
-    Tix createTix(Tix tix);
-}
