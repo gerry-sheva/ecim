5495c9f4c4579cd4463772140096c99b61500910
Gerry Sheva <gerry.sheva@proton.me>
2024-07-07 09:43:24 +0700

    feat(trx): stubbed test for trx

---
+++ src/test/java/com/dti/ecim/unit/trx/TrxServiceTest.java
@@ -0,0 +1,12 @@
+package com.dti.ecim.unit.trx;
+
+import com.dti.ecim.trx.service.TrxService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TrxServiceTest {
+    @Autowired
+    private TrxService trxService;
+
+}
