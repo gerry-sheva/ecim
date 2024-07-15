c8adb9b85b78552a29bd41297eecffc128b0979c
Gerry Sheva <gerry.sheva@proton.me>
2024-07-15 16:02:00 +0700

    feat(trx): added tests for trx service

--- src/test/java/com/dti/ecim/unit/trx/TrxServiceTest.java
+++ src/test/java/com/dti/ecim/unit/trx/TrxServiceTest.java
@@ -1,5 +1,6 @@
 package com.dti.ecim.unit.trx;
 
+import com.dti.ecim.exceptions.DataNotFoundException;
 import com.dti.ecim.trx.dto.CreateTixDto;
 import com.dti.ecim.trx.dto.CreateTrxRequestDto;
 import com.dti.ecim.trx.dto.TrxResponseDto;
@@ -7,9 +8,13 @@ import com.dti.ecim.trx.service.TrxService;
 import org.apache.coyote.BadRequestException;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.InvalidDataAccessApiUsageException;
+import org.springframework.security.test.context.support.WithUserDetails;
 
 import java.security.NoSuchAlgorithmException;
 import java.util.Set;
@@ -23,20 +28,67 @@ public class TrxServiceTest {
 
     @BeforeEach
     public void setUp() {
-        requestDto.setEventId(2L);
-        requestDto.setDiscountId(3L);
-
+        requestDto.setEventId(1L);
+        requestDto.setOrganizerId(1L);
         CreateTixDto tixDto = new CreateTixDto();
         tixDto.setQuantity(1);
         tixDto.setOfferingId(2L);
 
         requestDto.setTixes(Set.of(tixDto));
     }
+//    RETRIEVE TRX
+    @Test
+    @Disabled
+    @WithUserDetails("qweas@mail.com")
+    public void test_retrieve_trx_with_valid_id() throws BadRequestException, NoSuchAlgorithmException {
+        TrxResponseDto responseDto = trxService.retrieveTrx(302L);
+
+        Assertions.assertThat(responseDto).isNotNull();
+    }
 
     @Test
+    @WithUserDetails("qweas@mail.com")
+    public void test_retrieve_trx_with_invalid_id() throws BadRequestException, NoSuchAlgorithmException {
+        Assertions.assertThatThrownBy(() -> trxService.retrieveTrx(-1L)).isInstanceOf(DataNotFoundException.class);
+    }
+
+    @Test
+    @WithUserDetails("qweas@mail.com")
+    public void test_retrieve_trx_with_invalid_ownership() throws BadRequestException, NoSuchAlgorithmException {
+        Assertions.assertThatThrownBy(() -> trxService.retrieveTrx(2L)).isInstanceOf(DataNotFoundException.class);
+    }
+
+
+//    CREATE TRX
+    @Test
+    @WithUserDetails("qweas@mail.com")
     public void test_create_trx_with_valid_data() throws NoSuchAlgorithmException, BadRequestException {
         TrxResponseDto responseDto = trxService.createTrx(requestDto);
 
         Assertions.assertThat(responseDto).isNotNull();
+    }
+
+    @Test
+    @WithUserDetails("qweas@mail.com")
+    public void test_create_trx_with_invalid_organizer() throws BadRequestException, NoSuchAlgorithmException {
+        requestDto.setOrganizerId(-1L);
+
+        Assertions.assertThatThrownBy(() -> trxService.createTrx(requestDto)).isInstanceOf(DataIntegrityViolationException.class);
+    }
+
+    @Test
+    @WithUserDetails("qweas@mail.com")
+    public void test_create_trx_with_invalid_event() throws BadRequestException, NoSuchAlgorithmException {
+        requestDto.setEventId(-1L);
+
+        Assertions.assertThatThrownBy(() -> trxService.createTrx(requestDto)).isInstanceOf(DataIntegrityViolationException.class);
+    }
+
+    @Test
+    @WithUserDetails("qweas@mail.com")
+    public void test_create_trx_with_invalid_tix() throws BadRequestException, NoSuchAlgorithmException {
+        requestDto.setTixes(Set.of(new CreateTixDto()));
+
+        Assertions.assertThatThrownBy(() -> trxService.createTrx(requestDto)).isInstanceOf(InvalidDataAccessApiUsageException.class);
     }
 }
