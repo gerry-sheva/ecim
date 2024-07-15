043f1c452b6f96b5297d565cef5168b90fea9de9
Gerry Sheva <gerry.sheva@proton.me>
2024-07-15 14:53:50 +0700

    feat(trx): implemented validator to CreateTixDto

--- src/main/java/com/dti/ecim/trx/dto/CreateTixDto.java
+++ src/main/java/com/dti/ecim/trx/dto/CreateTixDto.java
@@ -1,5 +1,7 @@
 package com.dti.ecim.trx.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,6 +10,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CreateTixDto {
+    @Positive(message = "Offering id must be valid")
+    @NotNull(message = "Offering id must not be null")
     private Long offeringId;
+
+    @Positive(message = "Quantity must be valid")
+    @NotNull(message = "Quantity must not be null")
     private int quantity;
 }
