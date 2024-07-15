65fe9893b9af22ea32a3f9aa3cfce654084dc860
Gerry Sheva <gerry.sheva@proton.me>
2024-07-15 14:51:38 +0700

    feat(trx): implemented validator to CreateTrxRequestDto

--- src/main/java/com/dti/ecim/trx/dto/CreateTrxRequestDto.java
+++ src/main/java/com/dti/ecim/trx/dto/CreateTrxRequestDto.java
@@ -1,7 +1,9 @@
 package com.dti.ecim.trx.dto;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -10,17 +12,20 @@ import java.util.Set;
 @Data
 @NoArgsConstructor
 public class CreateTrxRequestDto {
-    @Positive
-    @NotNull
+    @Positive(message = "Event id must be valid")
+    @NotNull(message = "Event id can't be null")
     private Long eventId;
 
-    @Positive
+    @Positive(message = "Organizer id must be valid")
+    @NotNull(message = "Organzier id can't be null")
     private Long organizerId;
 
     private Long discountId;
 
     private boolean point;
 
-    @NotNull
+    @Size(min = 1, message = "Tickets can't be empty")
+    @NotNull(message = "Tickets can't be null")
+    @Valid
     private Set<CreateTixDto> tixes;
 }
