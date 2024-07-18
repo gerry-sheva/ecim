bf2736a372c423199ea541d3152a641ee3e00faa
Gerry Sheva <gerry.sheva@proton.me>
2024-07-18 14:36:25 +0700

    chore: cleaned up unused method

--- src/main/java/com/dti/ecim/discount/repository/DiscountRepository.java
+++ src/main/java/com/dti/ecim/discount/repository/DiscountRepository.java
@@ -1,7 +1,6 @@
 package com.dti.ecim.discount.repository;
 
 import com.dti.ecim.discount.entity.Discount;
-import com.dti.ecim.discount.entity.EventDiscount;
 import com.dti.ecim.discount.entity.EventDiscountDao;
 import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,7 +10,6 @@ import java.util.Optional;
 
 public interface DiscountRepository extends JpaRepository<Discount, Long> {
     Optional<Discount> findByCode(String code);
-//    EventDiscountDao findById(long id);
 
     @Query(value = "SELECT event_id from discount as d where d.id = :id", nativeQuery = true)
     EventDiscountDao retrieveEventDiscount(@Param("id") Long id);
