7494ad998437cebd14ad14c3c7c573be38d088f8
Gerry Sheva <gerry.sheva@proton.me>
2024-07-19 13:35:23 +0700

    feat(bucket): added format restriction for uploading image

--- src/main/java/com/dti/ecim/imageBucket/service/impl/ImageBucketServiceImpl.java
+++ src/main/java/com/dti/ecim/imageBucket/service/impl/ImageBucketServiceImpl.java
@@ -9,6 +9,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Service
@@ -17,10 +18,13 @@ public class ImageBucketServiceImpl implements ImageBucketService {
     @Resource
     private Cloudinary cloudinary;
 
+    private final List<String> ALLOWED_FORMAT = List.of("jpg", "jpeg", "png", "webp");
+
     @Override
     public String uploadImage(MultipartFile file, String folderName) throws IOException {
         HashMap<Object, Object> options = new HashMap<>();
         options.put("folder", folderName);
+        options.put("allowed_format", ALLOWED_FORMAT);
         Map uploadedFile = cloudinary.uploader().upload(file.getBytes(), options);
         String publicId = (String) uploadedFile.get("public_id");
         return cloudinary.url().secure(true).generate(publicId);
